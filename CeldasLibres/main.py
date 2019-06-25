c30347d4a33e8023b7b28d90b8d989f2c5892762
C4-T0 <carlostorresh1@gmail.com>
2019-06-25 00:06:48 -0500

    aun no funciona la conexion a la base de datos

--- CeldasLibres/main.py
+++ CeldasLibres/main.py
@@ -1,18 +1,31 @@
 from flask import Flask , render_template,request
-import mysql.connector as mariadb
-from flask_bootstrap import Bootstrap
-app = Flask(__name__)
-#mariadb_connection = mariadb.connect(host= 'localhost',port='3306', user='celdaslibres_dev', password='celdaslibres2019', database='celdas_libres')
+#import mysql.connector
+#import MySQLdb
+#from flask_bootstrap import Bootstrap
 #conexion=mysql.connector.connect(**dbconnect)
+#conexion = mysql.connector.connect(host="3.90.223.62",port="3306",user="app", password="VguGqXru53BV8kIW" , database="celdas_libres")
+#conexcion = MySQLdb.connect(host="3.90.223.62",port=3306,user="app",passwd="VguGqXru53BV8kIW",db="celdas_libres")
+app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
 @app.route('/crearUsuario')
 def crear_usuario():
     return render_template('crear_usuario.html')
-@app.route('/ingresar')
+
+
+@app.route('/login',  methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method =='POST':
+        id = request.form.get("id")
+        password = request.form.get("password")
+        info = str(id)+" "+str(password)
+        return (info)
+
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
