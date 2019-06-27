ec5987f924498d46fff0a750d683522122f1c896
Julian Rojas <julianrojasbet@gmail.com>
2019-06-26 21:43:01 -0500

    Registro de usuarios funcionando

--- celdas_libres/celdas_libres/urls.py
+++ celdas_libres/celdas_libres/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from core import views as core_views
 from django.urls import path, include
 
 urlpatterns = [
+    path('', core_views.home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
